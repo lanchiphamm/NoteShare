6f725f005530ddaec972095f4ef2e94189192a9c
nlmanama <nlminhanh12@gmail.com>
2023-08-26 08:58:55 +0700

    added login buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  var username = "";
+  var password = "";
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+  
+    const data =  Object.fromEntries(formData.entries());
+    console.log(data);
+    username = data.username;
+    password = data.password;
+    console.log(username);
+    console.log(password);
+
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "body">
+      <div className="heading">
+        <h1>Welcome to NoteShare</h1>
+      </div>
+
+      <div className = "login">
+        <h2>Login</h2>
+      </div>
+
+      <div className = "login-box">
+        <form method="login" onSubmit={handleSubmit}>
+          <label>
+            Username: <input name="username" />
+          </label>
+          <br />
+          <label>
+            Password: <input name="password" />
+          </label>
+          <br />
+          <button type="submit">Log in</button>
+        </form>
+      </div>
+
     </div>
   );
 }

635f0c124a8cb9e28119cff7766cc944075b261f
nlmanama <nlminhanh12@gmail.com>
2023-08-27 02:01:33 +0700

    results page

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,10 +1,89 @@
 import Navbar from "./Navbar";
+import './Main.css';
+import { useState } from 'react';
+
 
 function Main () {
+
+    const [subject, setSubject] = useState("CPSC");
+    const [number, setNumber] = useState(0);
+
+    const mathNumbers = [
+        {label: 100, value: 100},
+        {label: 101, value: 101},
+    ]
+
+    const cpscNumbers = [
+        {label: 103, value: 103},
+        {label: 110, value: 110},
+    ]
+
+    const statNumbers = [
+        {label: 200, value: 200},
+        {label: 201, value: 201},
+    ]
+
+    function returnArray(s) {
+        switch (s) {
+            case "CPSC":
+                return cpscNumbers;
+            case "MATH":
+                return mathNumbers;
+            case "STAT":
+                return statNumbers;
+        }
+    }
+
+    function handleSubject(e) {
+        setSubject(e.target.value);
+        setNumber(returnArray(e.target.value)[0].value);
+    }
+    
+    function handleNumber(e) {
+        setNumber(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        window.location.replace("/result");
+    }
+
     return (
         <>
             <Navbar />
-            <h1>Main Page</h1>
+            <h1>Course Selection</h1>
+
+            <div className="subject">
+                <h2>Subject</h2>
+            </div>
+            <div className="select-course">
+                <label>
+                    Select Course Code
+                    <select value={subject} onChange={handleSubject}>
+                        <option value="CPSC">CPSC</option>
+                        <option value="MATH">MATH</option>
+                        <option value="STAT">STAT</option>
+                    </select>
+                </label>
+            </div>
+
+            <div className="select-number">
+                <label>
+                    Select Course Number
+                    <select value={number} onChange={handleNumber}>
+                        
+                        {returnArray(subject).map((option) => (
+                            <option value={option.value}>{option.label}</option>
+                        ))}
+    
+                    </select>
+                </label>
+            </div>
+
+            <div className ="submit-btn">
+                 <button onClick={handleSubmit}>Submit</button>
+            </div>
+
+            
         </>
     );
 }
---
+++ frontend/src/Result.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+
+function Result () {
+    return (
+        <>
+            <Navbar />
+            <h1>Result Page</h1>
+        </>
+    );
+}
+
+export default Result;

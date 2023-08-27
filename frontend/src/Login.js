2a69986de4f690268a851f0267a2b69725608157
nlmanama <nlminhanh12@gmail.com>
2023-08-27 12:01:23 +0700

    data passing lets gooo

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,11 +1,13 @@
 import './Login.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Popup from './components/Popup.js';
 
 
 function Login() {
-  var username = "";
-  var password = "";
+  // var username = "";
+  // var password = "";
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
   function handleSubmit(e) {
@@ -14,9 +16,8 @@ function Login() {
     const formData = new FormData(form);
   
     const data =  Object.fromEntries(formData.entries());
-    username = data.username;
-    password = data.password;
-    console.log(username);
+    setUsername(data.username);
+    setPassword(data.password);
     var userCorrect = checkPassword(username, password);
     
     if (userCorrect) {
@@ -33,6 +34,10 @@ function Login() {
   function signUp() {
     window.location.replace("/signup");
   }
+
+  useEffect(() => {
+    window.localStorage.setItem('USERNAME', username)
+  }, [username])
 
   return (
     <div className = "body">
--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,10 +1,20 @@
 import Navbar from "./Navbar";
+import { useState, useEffect } from 'react';
 
 function Profile () {
+
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        const data = window.localStorage.getItem('USERNAME');
+        if ( data !== null ) setUsername(data)
+    }, [])
+
     return (
         <>
             <Navbar />
             <h1>Profile Page</h1>
+            <h2>{username}</h2>
         </>
     );
 }
